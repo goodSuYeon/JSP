c339c26cbff2bf853a200cf2f80ef3b7012ccc3e
suyeon Kim <dutlstorgl@naver.com>
2020-12-28 11:04:34 +0900

    - maven 설정 - logback 설정 - basic servlet
    - url mapping에 따른 서블릿 실행과정

--- src/main/java/kr/or/ddit/servlet/basic/BasicServlet.java
+++ src/main/java/kr/or/ddit/servlet/basic/BasicServlet.java
@@ -2,22 +2,51 @@ package kr.or.ddit.servlet.basic;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class BasicServlet extends HttpServlet {
+
+	private static final Logger logger = LoggerFactory.getLogger(BasicServlet.class);
 
 	//doXXX 메소드 인자 : req, res
 	//GET, POST, DELETE, PUSH, HEAD ... : HTTP메소드
+	public void init() throws ServletException{
+		System.out.println("basicServlet.init()");
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//재정의
+		logger.debug("basicServlet.doGet()");
+		
+		/* 만약에 log level를 debug보다 높은 레벨로 설정할 경우
+		   로그를 생성하지는 않지만, 메소드 인자를 문자열+문자열 -> 문자열 결합 연산을 발생한다.
+		*/ 
+		logger.debug("basicServlet.doGet() userid paramter: {} {}" ,
+				req.getParameter("userid") , req.getParameter("password"));
+		
+		resp.setContentType("text/html; charset=utf-8");
+		
+		//servlet의 라이프 사이클
+		
 		PrintWriter out = resp.getWriter();
-		out.println("Hello, World");
+		out.println("<html>");
+		out.println("	<head>");
+		out.println("		<title>basic Servlet</title>");
+		out.println("	</head>");
+		out.println("<body>");
+		out.println("	<h2>나는 김뚜욘!</h2>");
+		out.println("</body>");
+		out.println("</html>");
+		out.println("Hello, World" + new Date());
 		
 		out.flush();	//더이상 작성할 내용이 없으므로 작업을 마무리한다.
 		out.close();	//그리고, 사용할 자원 반납한다.
